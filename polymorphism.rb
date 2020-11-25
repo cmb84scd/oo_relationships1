8e5b1306fd4f3c74a9c7f50e6f845d38c3fb25a6
cmb84scd <cmb84scd@gmail.com>
2020-11-25 17:24:42 +0000

    Fix polymorphism exercise

--- polymorphism.rb
+++ polymorphism.rb
@@ -3,55 +3,51 @@ class ScrambledDiary
     @contents = contents
   end
 
-  def read(content)
-    @contents = content.scramble
+  def read
+    @contents
+  end
+
+  def scramble(scrambler)
+    @contents = scrambler.scramble(@contents)
   end
 end
 
 class ScrambleAdvanceChars
-  def initialize(contents)
-    @contents = contents
+  def initialize(steps)
+    @steps = steps
   end
 
-  def scramble(steps)
-    plain_chars = @contents.chars
+  def scramble(contents)
+    plain_chars = contents.chars
     scrambled_chars = plain_chars.map do |char|
-      (char.ord + steps).chr
+      (char.ord + @steps).chr
     end
-    @contents = scrambled_chars.join
+    contents = scrambled_chars.join
   end
 end
 
 class UnscrambleAdvanceChars
-  def initialize(contents)
-    @contents = contents
+  def initialize(steps)
+    @steps = steps
   end
 
-  def scramble(steps)
-    scrambled_chars = @contents.chars
+  def scramble(contents)
+    scrambled_chars = contents.chars
     plain_chars = scrambled_chars.map do |char|
-      (char.ord - steps).chr
+      (char.ord - @steps).chr
     end
-    @contents = plain_chars.join
+    contents = plain_chars.join
   end
 end
 
 class ScrambleReverse
-  def initialize(contents)
-    @contents = contents
-  end
-
-  def scramble
-    @contents = @contents.reverse
+  def scramble(contents)
+    contents = contents.reverse
   end
 end
 
 class UnscrambleReverse
-  def initialize(contents)
-    @contents = contents
-  end
-
-  def scramble
-    @contents = @contents.reverse
+  def scramble(contents)
+    contents = contents.reverse
   end
 end
